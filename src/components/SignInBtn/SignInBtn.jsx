199055a9c871828a2f299c8e884c0db4f355b737
odayTelbany <oday0595814990@gmail.com>
2023-08-03 20:06:07 +0300

    provider.name toLowerCase

--- src/components/SignInBtn/SignInBtn.jsx
+++ src/components/SignInBtn/SignInBtn.jsx
@@ -5,7 +5,7 @@ import { signIn } from 'next-auth/react';
 const SignInBtn = ({ name }) => {
   return (
     <button
-      onClick={() => signIn(name)}
+      onClick={() => signIn(name.toLowerCase())}
       className="flex items-center gap-4 shadow-xl rounded-lg pl-3"
     >
       <span className="bg-blue-500 text-white px-4 py-3">
