e9dd9a298a1c8a4f8401b92862a0fffaa5a593a1
odayTelbany <oday0595814990@gmail.com>
2023-08-05 12:08:15 +0300

    miniprofile new style

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,25 +1,30 @@
+"use client"
 import React from 'react'
 import Stories from '../Stories/Stories'
 import Posts from '../Posts/Posts'
 import MiniProfile from '../MiniProfile/MiniProfile'
 import Suggestions from '../Suggestions/Suggestions'
 import SignInBtn from '../SignInBtn/SignInBtn'
+import { useSession } from 'next-auth/react'
 
 const Feed = () => {
+  const {data: session} = useSession();
   return (
-    <main className='lg:px-24 grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+    <main className={`lg:px-24 grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
         <section className='col-span-2'>
             {/* <SignInBtn /> */}
             <Stories />
             <Posts />
         </section>
 
+        {session && 
         <section className='hidden xl:inline md:col-span-1'>
           <div className=''>
             <MiniProfile />
             <Suggestions />
           </div>
         </section>
+        }
     </main>
   )
 }
