d33e25cf898f9b8065daf9ea3dfb9780e646d7d7
odayTelbany <oday0595814990@gmail.com>
2023-08-07 22:49:02 +0300

    Sign in page

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -14,10 +14,12 @@ export default function SignIn() {
   const providers = use(getData());
 
   return (
-    <>
+    <div className='flex flex-col justify-center items-center'>
+      <img src='https://links.papareact.com/ocw' className='w-40 mt-12'/>
+      <p className='w-2/5 text-center mb-16 mt-4'>The best photos, videos and stories application with the new reels feature, created by meta</p>
       {Object.values(providers).map(provider => {
         return <SignInBtn name={provider.name} key={provider.id} />;
       })}
-    </>
+    </div>
   );
 }
--- src/components/SignInBtn/SignInBtn.jsx
+++ src/components/SignInBtn/SignInBtn.jsx
@@ -6,9 +6,9 @@ const SignInBtn = ({ name }) => {
   return (
     <button
       onClick={() => signIn(name.toLowerCase(), {callbackUrl: "/"})}
-      className="flex items-center gap-4 shadow-xl rounded-lg pl-3"
+      className="bg-blue-500 flex items-center gap-4 shadow-xl rounded-lg pl-3"
     >
-      <span className="bg-blue-500 text-white px-4 py-3">
+      <span className=" text-white px-4 py-3 w-full">
         Sign in with {name}
       </span>
     </button>
