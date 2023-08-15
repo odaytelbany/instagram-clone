4ebcbc6fd3c17eb039e65ed494022dbddce7cdee
odayTelbany <oday0595814990@gmail.com>
2023-08-15 16:20:57 +0300

    save work to deploy

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,7 +4,6 @@ import Stories from '../Stories/Stories'
 import Posts from '../Posts/Posts'
 import MiniProfile from '../MiniProfile/MiniProfile'
 import Suggestions from '../Suggestions/Suggestions'
-import SignInBtn from '../SignInBtn/SignInBtn'
 import { useSession } from 'next-auth/react'
 
 const Feed = () => {
@@ -12,7 +11,6 @@ const Feed = () => {
   return (
     <main className={`lg:px-24 grid grid-cols-1 md:grid-cols-2 md:max-w-lg lg:max-w-3xl xl:grid-cols-3 xl:max-w-5xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
         <section className='col-span-2'>
-            {/* <SignInBtn /> */}
             <Stories />
             <Posts />
         </section>
